703d5f4ac20c2a3e0c9ad9d8962f4119e65092dd
Chelsea Dyck <chelseadyck@cassconsultinc.ca>
2020-10-21 11:58:31 -0600

    established all component files and initial set up of client and server folders

---
+++ server/knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'pet_adoption_capstone',
+        charset: 'utf8'
+    }
+}
