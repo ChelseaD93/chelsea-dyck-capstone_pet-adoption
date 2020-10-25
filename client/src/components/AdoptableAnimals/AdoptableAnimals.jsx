cfd2e74730dcc0b5a4331a907562667004d58cad
Chelsea Dyck <chelseadyck@cassconsultinc.ca>
2020-10-25 10:59:57 -0600

    worked out token access to PetFinder API, now able to receive token

--- client/src/components/AdoptableAnimals/AdoptableAnimals.jsx
+++ client/src/components/AdoptableAnimals/AdoptableAnimals.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
+import axios from "axios";
 import AdoptableCats from '../AdoptableCats/AdoptableCats';
 import AdoptableCritters from '../AdoptableCritters/AdoptableCritters';
 import AdoptableDogs from '../AdoptableDogs/AdoptableDogs';
 
-function AdoptableAnimals(props) {
-    return (
+class AdoptableAnimals extends React.Component {
+
+
+    componentDidMount() {
+    //information for API access through PetFinder.com
+    const API_KEY = 'Ab8xQ5x4ko6Ue4EfS12b2T4jmijVdGltbWXPJ8zLreFMbC9Lo1';
+    const SECRET_KEY = 'vswXi2YoIUPufT0BrJCtRXoXKkJI8gEn16QehuXn';
+
+    axios
+        .post('https://api.petfinder.com/v2/oauth2/token',
+            {
+                grant_type:'client_credentials',
+                client_id: API_KEY,
+                client_secret: SECRET_KEY
+            }
+        )
+        .then((res) => {
+            console.log(res.data.access_token)
+        })
+    }
+    //componentDidUpdate -- refreshing timed axios to keep up with token timeouts
+
+
+    render() {
+        return (
         <div>
 
             <Switch>
@@ -14,7 +38,8 @@ function AdoptableAnimals(props) {
                 <Route path='/adoptable-animals/critters' exact render={(routeProps) => <AdoptableCritters {...routeProps} />} />
             </Switch>            
         </div>
-    );
+        );
+    }
 }
 
 export default AdoptableAnimals;
--- server/routes/PetFinder.js
+++ server/routes/PetFinder.js
@@ -1,2 +1,28 @@
-//api from pet finder : Ab8xQ5x4ko6Ue4EfS12b2T4jmijVdGltbWXPJ8zLreFMbC9Lo1
-//secret from pet finder: vswXi2YoIUPufT0BrJCtRXoXKkJI8gEn16QehuXn
+const express = require('express');
+const app = express();
+const AdopterProfiles = require('../database/models/adopter_profiles');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+//information for API access through PetFinder.com
+const API_KEY = Ab8xQ5x4ko6Ue4EfS12b2T4jmijVdGltbWXPJ8zLreFMbC9Lo1;
+const SECRET_KEY = vswXi2YoIUPufT0BrJCtRXoXKkJI8gEn16QehuXn;
+
+//call to get token to access data --- expires every 3600
+route
+    .post('https://api.petfinder.com/v2/oauth2/token',
+        {
+            grant_type:'client_credentials',
+            client_id: API_KEY,
+            client_secret: SECRET_KEY
+        }
+    )
+    .then((res) => {
+        authToken = res.data.access_token
+    })
+
+    
+    
+module.exports = router;
