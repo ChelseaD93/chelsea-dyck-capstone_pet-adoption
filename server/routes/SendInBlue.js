e37645ea1663c1fe56d7b77f5728c44684bc3c22
Chelsea Dyck <chelseadyck@cassconsultinc.ca>
2020-10-25 19:18:30 -0600

    added suggested node code for sending email to SendInBlue route

--- server/routes/SendInBlue.js
+++ server/routes/SendInBlue.js
@@ -1,1 +1,18 @@
 //api key from SendInBlue : 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
+
+
+//send transactional email --- this one is for the welcome email
+// const fetch = require('node-fetch');
+
+// let url = 'https://api.sendinblue.com/v3/smtp/email';
+
+// let options = {
+//   method: 'POST',
+//   headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+//   body: '{"sender":{"name":"Chelsea","email":"chelsea@mycreativeworks.ca"},"to":[{"email":"h@email.com","name":"bob bbbb"}],"params":{"firstName":"New Value","lastName":"New Value"},"templateId":1}'
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error('error:' + err));
